168263d1e3a609626caaefe9f40dfb0e15b7e411
zerokinetic <mhemansakkthivel@gmail.com>
2025-08-28 11:17:09 +0530

    added startFileServer

--- server/src/main/java/peershare/services/FileSharer.java
+++ server/src/main/java/peershare/services/FileSharer.java
@@ -2,6 +2,10 @@ package peershare.services;
 
 import peershare.utils.UploadUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 
 public class FileSharer {
@@ -22,6 +26,26 @@ public class FileSharer {
     }
 
     public void startFileServer(int port) {
-        //Logic goes here
+        String filePath = availableFiles.get(port);
+        if (filePath.isEmpty()) {
+            System.out.println("No files associated with that port: " + port);
+            return;
+        }
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Serving File " + new File(filePath).getName() + " on port " + port);
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client Connection: " + clientSocket.getInetAddress());
+//            new Thread(new FileSenderHandler(clientSocket, filePath)).start();
+        } catch (IOException ex) {
+            System.out.println("Error Handling FileServer on port: " + port);
+        }
+    }
+
+    private static class FileSenderHandler implements Runnable {
+        @Override
+        public void run() {
+            return;
+        }
     }
 }
